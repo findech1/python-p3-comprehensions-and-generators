d1ef4badd92940e6e8331bf381fd1074b7704c3b
Felix <findech6@gmail.com>
2023-11-24 00:14:51 +0300

    completed assignments

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
 
-def return_evens(num_list):
-    pass
+def return_evens(sequence):
+    even_numbers = [num for num in sequence if num % 2 == 0]
+    return even_numbers
 
-def make_exclamation(sentence_list):
-    pass
+# Example usage:
+sequence = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+result = return_evens(sequence)
+print(result)
+
+#exclamation
+def make_exclamation(sentences):
+    sentences_with_exclamation = [sentence + '!' for sentence in sentences]
+    return sentences_with_exclamation
+
+# Example usage:
+sentence_list = ["Hello", "How are you", "Have a great day"]
+result = make_exclamation(sentence_list)
+print(result)
